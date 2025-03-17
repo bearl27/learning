8b3a1b3b1660ddb67bdc4cf67cc961ce5f835223
bearl27 <nagahari.kai@hrbrain.co.jp>
2025-03-17 13:15:46 +0900

    dir

--- goroutine/main.go
+++ goroutine/main.go
@@ -1,29 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
-
-func sayHello(wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println("Hello")
-	time.Sleep(1 * time.Second)
-}
+import "goroutine/practices"
 
 func main() {
-	var wg sync.WaitGroup
-	wg.Add(1)
-
-	go sayHello(&wg)
-	wg.Add(1)
-
-	go func() {
-		defer wg.Done()
-		fmt.Println("Bye")
-	}()
-	fmt.Println("waiting")
-	wg.Wait()
-	fmt.Println("End")
+	practices.N_170()
 }
